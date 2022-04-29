e7f5704b0d739d35d9424ffd4c957620d848309b
Leonard <Leonard@DESKTOP-361U0BR>
2022-04-30 05:14:53 +0800

    hehe

--- views/admin/orders.php
+++ views/admin/orders.php
@@ -42,7 +42,7 @@ if(isset($_GET['st'])){
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 		
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> 
 		<link rel="stylesheet" href="../../css/style.css">
   </head>
   <body>
